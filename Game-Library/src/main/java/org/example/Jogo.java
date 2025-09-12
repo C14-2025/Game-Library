03c77196a89d2438455e6527a87afd221d5dba47
Lucas <lucas.caetano.reis@hotmail.com>
2025-09-12 17:06:23 -0300

    Feat: Criei as classes Jogo, BibliotecaJogo e Main.

--- Game-Library/src/main/java/org/example/Jogo.java
+++ Game-Library/src/main/java/org/example/Jogo.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class Jogo {
+    private String nome;
+    private String dataLancamento;
+    private String genero;
+    private int duracao;
+
+    public Jogo(String nome, String dataLancamento, String genero, int duracao) {
+        this.nome = nome;
+        this.dataLancamento = dataLancamento;
+        this.genero = genero;
+        this.duracao = duracao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
 }
