88939d031252f5707fad2ee169adb84a1cc9bb72
Lucas <lucas.caetano.reis@hotmail.com>
2025-09-17 19:10:18 -0300

    Refactor: Corrigido problema de sintaxe no método adicionarJogo.

--- Game-Library/src/main/java/org/example/BibliotecaJogo.java
+++ Game-Library/src/main/java/org/example/BibliotecaJogo.java
@@ -23,8 +23,7 @@ public class BibliotecaJogo {
     }
 
     public void adicionarJogo(Jogo jogo){
-        boolean existe = jogos.stream()
-                .anyMatch(j -> j.getNome().equalsIgnoreCase(jogo.getNome()));
+        boolean existe = jogos.stream().anyMatch(j -> j.getNome().equalsIgnoreCase(jogo.getNome()));
         if (existe) {
             throw new IllegalArgumentException("O jogo '" + jogo.getNome() + "' já existe na biblioteca.");
         }
